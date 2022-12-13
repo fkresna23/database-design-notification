25754f626f7c623cc9d62247eb808a5debfac5a1
f.kresna23 <f.kresna23@gmail.com>
2022-12-13 11:12:36 +0700

    Design Database Category

--- notification.sql
+++ notification.sql
@@ -62,3 +62,75 @@ SELECT *
 FROM notification
 WHERE (user_id = 'pahlawan' OR user_id IS NULL)
 ORDER BY create_at DESC;
+
+# Category
+
+CREATE TABLE category
+(
+    id   VARCHAR(100) NOT NULL,
+    name VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+SHOW TABLES;
+
+ALTER TABLE notification
+    ADD COLUMN category_id VARCHAR(100);
+
+DESCRIBE notification;
+
+ALTER TABLE notification
+    ADD CONSTRAINT fk_notification_category
+        FOREIGN KEY (category_id) REFERENCES category (id);
+
+DESC notification;
+
+SELECT *
+FROM notification;
+
+INSERT INTO category(id, name)
+VALUES ('INFO', 'Info');
+INSERT INTO category(id, name)
+VALUES ('PROMO', 'Promo');
+
+SELECT *
+FROM category;
+
+UPDATE notification
+SET category_id = 'INFO'
+WHERE id = 1;
+
+UPDATE notification
+SET category_id = 'PROMO'
+WHERE id = 2;
+
+UPDATE notification
+SET category_id = 'INFO'
+WHERE id = 3;
+
+SELECT *
+FROM notification;
+
+SELECT *
+FROM notification
+WHERE (user_id = 'kresna' OR user_id IS NULL)
+ORDER BY create_at DESC;
+
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'kresna' OR n.user_id IS NULL)
+ORDER BY n.create_at DESC;
+
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'pahlawan' OR n.user_id IS NULL)
+ORDER BY n.create_at DESC;
+
+SELECT *
+FROM notification n
+         JOIN category c ON (n.category_id = c.id)
+WHERE (n.user_id = 'pahlawan' OR n.user_id IS NULL)
+  AND c.id = 'INFO'
+ORDER BY n.create_at DESC;
