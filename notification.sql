756e6b3c1adc3b1ba07642194e542cd98c3aa455
f.kresna23 <f.kresna23@gmail.com>
2022-12-13 11:01:01 +0700

    Design Database Notification

--- notification.sql
+++ notification.sql
@@ -22,3 +22,43 @@ VALUES ('pahlawan', 'Fajar Pahlawan');
 
 SELECT *
 FROM user;
+
+# Notification
+
+CREATE TABLE notification
+(
+    id        INT          NOT NULL AUTO_INCREMENT,
+    title     VARCHAR(255) NOT NULL,
+    detail    TEXT         NOT NULL,
+    create_at TIMESTAMP    NOT NULL,
+    user_id   VARCHAR(100),
+    PRIMARY KEY (id)
+) ENGINE = InnoDB;
+
+SHOW TABLES;
+
+ALTER TABLE notification
+    ADD CONSTRAINT fk_notification_user
+        FOREIGN KEY (user_id) REFERENCES user (id);
+
+DESC notification;
+
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Pesanan', 'Detail Pesanan', CURRENT_TIMESTAMP(), 'kresna');
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Promo', 'Detail Promo', CURRENT_TIMESTAMP(), null);
+INSERT INTO notification(title, detail, create_at, user_id)
+VALUES ('Contoh Pembayaran', 'Detail Pembayaran', CURRENT_TIMESTAMP(), 'pahlawan');
+
+SELECT *
+FROM notification;
+
+SELECT *
+FROM notification
+WHERE (user_id = 'kresna' OR user_id IS NULL)
+ORDER BY create_at DESC;
+
+SELECT *
+FROM notification
+WHERE (user_id = 'pahlawan' OR user_id IS NULL)
+ORDER BY create_at DESC;
